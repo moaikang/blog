b52295cc291e2121bd58c8569258c5d786bd1c06
moaikang <kk_w1207@naver.com>
2021-01-15 21:41:32 +0900

    [BE] Feat : :zap:️ Router을 App과 연결, cors 모듈 적용 #8
    초기 설정을 마친 라우터를 App과 연결하였다.
    cors 모듈을 적용하였다.

--- server/src/initializer/index.ts
+++ server/src/initializer/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import envInitializer from './env';
+import expressInitializer from './express';
 import mongoInitializer from './mongo';
 
 export default async (expressApp: express.Application): Promise<void> => {
   envInitializer();
-  mongoInitializer();
+  expressInitializer(expressApp);
+  await mongoInitializer();
 };
